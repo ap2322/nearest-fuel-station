68f17d0c5df35a8043b1852bc9f444e49fc1ec0a
Alice Post <alicep2322@gmail.com>
2019-12-20 11:08:49 -0700

    Update findstation

--- app/poros/find_station.rb
+++ app/poros/find_station.rb
@@ -1,12 +1,14 @@
 class FindStation
+  attr_reader :nearest_station
+  
   def initialize(address)
     @location_address = address
-
+    @nearest_station = fetch_nearest_station
   end
 
   def fetch_nearest_station
     service = NRELService.new
-    station_info = service.nearest_station(@location_address)
+    station_info ||= service.nearest_station(@location_address)
     Station.new(station_info[:fuel_stations][0])
   end
 end
