2c6447f0771e57b7dad4cffe28c5f82caa8dc59a
Alice Post <alicep2322@gmail.com>
2019-12-20 11:08:58 -0700

    Update search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     station = FindStation.new(params[:location])
-    render locals: {station: station.fetch_nearest_station}
+    render locals: {station: station.nearest_station}
   end
 end
