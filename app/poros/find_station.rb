81d50287b80324f2b80cff491b2eaba1730c5123
Alice Post <alicep2322@gmail.com>
2019-12-20 10:35:00 -0700

    Update find station poro to initialize station

--- app/poros/find_station.rb
+++ app/poros/find_station.rb
@@ -1,7 +1,12 @@
 class FindStation
   def initialize(address)
     @location_address = address
+
   end
 
-  
+  def fetch_nearest_station
+    service = NRELService.new
+    station_info = service.nearest_station(@location_address)
+    Station.new(station_info[:fuel_stations][0])
+  end
 end
